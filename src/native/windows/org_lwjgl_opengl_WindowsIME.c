e8cfa2015fc6bdc22064d617a97d18daeac439ae
Mark Johnson <mjohnson@threerings.net>
2013-08-07 18:21:37 -0700

    Fix this so it compiles

--- src/native/windows/org_lwjgl_opengl_WindowsIME.c
+++ src/native/windows/org_lwjgl_opengl_WindowsIME.c
@@ -1,20 +1,17 @@
 #include "Windows.h"
 #include <malloc.h>
-#include <jni.h>
 #include "org_lwjgl_opengl_WindowsIME.h"
 
 #define STRINGRETURNER_SIG "org/lwjgl/opengl/WindowsIME$StringReturner"
 
 jobject getStringReturner (JNIEnv *env, jint result, jstring buf)
 {
-    jclass clazz = (*env)->FindClass(STRINGRETURNER_SIG);
-    jmethodID constructor = (*env)->GetMethodID(clazz, "<init>", "()V");
-    jobject object = (*env)->GetFieldID(clazz, constructor);
-    jfieldID resultFieldId = (*env)->GetFieldID(clazz, "result", "I");
-    jfieldID bufFieldId = (*env)->GetFieldID(clazz, "buf", "Ljava/lang/String;");
+    jclass clazz = (*env)->FindClass(env, STRINGRETURNER_SIG);
+    jmethodID constructor = (*env)->GetMethodID(env, clazz, "<init>", "()V");
+    jobject object = (*env)->NewObject(env, clazz, constructor);
 
-    (*env)->SetObjectField(object, resultFieldId, result);
-    (*env)->SetObjectField(object, bufFieldlId, buf);
+    (*env)->SetIntField(env, object, (*env)->GetFieldID(env, clazz, "result", "I"), result);
+    (*env)->SetObjectField(env, object, (*env)->GetFieldID(env, clazz, "buf", "Ljava/lang/String;"), buf);
 
     return object;
 }
@@ -46,14 +43,16 @@ JNIEXPORT jobject JNICALL Java_org_lwjgl_opengl_WindowsIME_ImmGetCompositionStri
     HIMC himc = (HIMC)(INT_PTR)himc_int;
     DWORD dwIndex = (DWORD)dw_index_int;
     LONG result = ImmGetCompositionString(himc, dwIndex, (LPVOID)NULL, 0l);
+    LPVOID buf;
+    jstring str;
 
-    if (dwIndex == GCS_CURSOR_POS || dwIndex == GCS_DELTASTART ||
+    if (dwIndex == GCS_CURSORPOS || dwIndex == GCS_DELTASTART ||
             result == IMM_ERROR_NODATA || result == IMM_ERROR_GENERAL) {
         return getStringReturner(env, (jint)result, (jstring)NULL);
     }
-    LPVOID buf = (LPVOID)malloc(result);
+    buf = malloc(result);
     result = ImmGetCompositionString(himc, dwIndex, buf, result);
-    jstring str (*env)->NewStringUTF(env, buf);
+    str = (*env)->NewStringUTF(env, buf);
     free(buf);
     return getStringReturner(env, (jint)result, str);
 }
